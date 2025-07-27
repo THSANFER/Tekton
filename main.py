81a83b14a2bf772e8c4cde06962620fe19595e1b
THSANFER <50412296+THSANFER@users.noreply.github.com>
2025-07-26 22:13:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,22 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 import database as db
+import utils # Adicione esta importação
 from views.main_window import MainWindow
 
 if __name__ == '__main__':
-    # 1. Inicializa o banco de dados
+    # 1. Garante que a pasta base 'Tektõn - Clientes' exista
+    utils.setup_base_folder()
+    
+    # 2. Inicializa o banco de dados
     db.init_db()
     
-    # 2. Cria a aplicação
+    # 3. Cria a aplicação
     app = QApplication(sys.argv)
     
-    # 3. Cria e exibe a janela principal
+    # 4. Cria e exibe a janela principal
     window = MainWindow()
     window.show()
     
-    # 4. Inicia o loop de eventos da aplicação
+    # 5. Inicia o loop de eventos da aplicação
     sys.exit(app.exec_())
